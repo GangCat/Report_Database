a155215f04c0a0800b5215ec68cd56fc24b0700f
GangCat <windy9300@gmail.com>
2023-08-22 18:21:30 +0900

    RETURN BUTTON IN RANK

--- Assets/Scripts/UI/UI_State_Rank.cs
+++ Assets/Scripts/UI/UI_State_Rank.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_State_Rank : MonoBehaviour
 {
     public void SetActive(bool _isActive)
     {
         gameObject.SetActive(_isActive);
+    }
+
+    public void Init(VoidVoidDelegate _retryBtnCallback)
+    {
+        btnRetry.onClick.AddListener(
+        () =>
+        {
+            _retryBtnCallback?.Invoke();
+        }
+        );
     }
 
     public void ShowRanking(List<SDataScore> _listDataScore)
@@ -19,4 +30,6 @@ public class UI_State_Rank : MonoBehaviour
 
     [SerializeField]
     private UI_State_Rank_ScrollView uiRankScrollView = null;
+    [SerializeField]
+    private Button btnRetry = null;
 }
